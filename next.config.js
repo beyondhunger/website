d0c5187025e1bbddf2fb2c7e3cbc29d57d3b79a6
Shiva Krishna <shivakrishna@Shivas-MacBook-Pro.local>
2025-11-26 19:52:06 +0000

    Fix Next.js outputFileTracingIncludes location

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    outputFileTracingIncludes: {
-      // include prisma engine inside serverless bundle
-      "src/app/api/**": ["./node_modules/.prisma/client/**/*"]
-    },
-  },
+  outputFileTracingIncludes: {
+    '/api/services': ['./node_modules/.prisma/**/*']
+  }
 };
 
 module.exports = nextConfig;
